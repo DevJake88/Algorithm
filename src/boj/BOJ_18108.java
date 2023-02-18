a7f49463c20b8640a576c17c1fd0df241bb5f9c2
DevJake88 <developer.jake88@gmail.com>
2023-02-18 13:50:49 +0900

    Add BOJ_18108

---
+++ src/boj/BOJ_18108.java
@@ -0,0 +1,14 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_18108 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int y = scan.nextInt();
+		int solution = y - (2541-1998);
+		System.out.println(solution);
+	}
+
+}
