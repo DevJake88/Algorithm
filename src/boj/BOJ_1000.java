a377c2397c91fa03b9515a53e08b477ecf5b5d81
DevJake88 <developer.jake88@gmail.com>
2023-02-18 11:56:46 +0900

    Add BOJ_1000

---
+++ src/boj/BOJ_1000.java
@@ -0,0 +1,14 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_1000 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		System.out.println(A+B);
+	}
+
+}
