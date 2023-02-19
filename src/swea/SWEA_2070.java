acef55f798ef4ab1b98b0556991e1b2c398e9bca
DevJake88 <developer.jake88@gmail.com>
2023-02-19 09:29:10 +0900

    SWEA_2070

---
+++ src/swea/SWEA_2070.java
@@ -0,0 +1,27 @@
+package swea;
+
+import java.util.Scanner;
+
+public class SWEA_2070 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int T = scan.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int num1 = scan.nextInt();
+			int num2 = scan.nextInt();
+
+			char result;
+			if (num1 > num2)
+				result = '>';
+			else if (num1 == num2)
+				result = '=';
+			else
+				result = '<';
+
+			System.out.println("#" + (i + 1) + " " + result);
+		}
+	}
+
+}
