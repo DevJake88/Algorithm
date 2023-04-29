7a950f984fa22a16909acb5f445e68ebeabc917d
DevJake88 <developer.gmail.com>
2023-04-29 20:38:48 +0900

    Programmers_나머지가1이되는수찾기

---
+++ src/programmers/lv1/Programmers_나머지가1이되는수찾기.java
@@ -0,0 +1,23 @@
+package programmers.lv1;
+
+public class Programmers_나머지가1이되는수찾기 {
+	
+	public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=2; i<1000000; i++) {
+            if (n%i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		Programmers_나머지가1이되는수찾기 test = new Programmers_나머지가1이되는수찾기();
+		int result = test.solution(10);
+		System.out.println(result);
+	}
+	
+}
